74d591376424878e78cdd3258d28ea01c41cc708
vasiljevic-nikola <vasiljevicnikolabps@gmail.com>
2025-08-07 09:38:07 +0200

    feat: Create InteractableObject script

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    // Definišemo tipove objekata koje imamo.
+    // Ovo će nam se pojaviti kao padajući meni u Unity-ju.
+    public enum ObjectType
+    {
+        Book,
+        Lamp,
+        Laptop
+    }
+
+    // Ovde biramo koji je ovo tip objekta.
+    public ObjectType type;
+
+    // Ovde štikliramo da li je objekat opasan (Game Over ako ga dodirnemo).
+    public bool isDangerous;
+
+}
+
